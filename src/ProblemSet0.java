b90b8492a8a0792e05ea901a4f383b3e67300160
cvasquez2020 <caalevas@gmail.com>
2019-09-17 20:07:24 -0400

    Update ProblemSet0.java

--- src/ProblemSet0.java
+++ src/ProblemSet0.java
@@ -1,16 +1,16 @@
-  
+
 /**
  * Problem Set 0.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on printing
  * text to the console, as well as identifying and correcting compilation and
  * runtime errors in the supplied program code.
- * 
+ *
  * The specifications for each exercise are outlined below. I've written most of
  * the functionality for you, but unfortunately, my code is riddled with mistakes.
  * It's your job to fix the errors I've made, so that the code meets the
  * requirements set forth in this section (as well as the Deliverables section).
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
@@ -21,7 +21,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 1.
-         * 
+         *
          * Print a message to the console.
          */
 
@@ -29,7 +29,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 2.
-         * 
+         *
          * Print APCS in block letters to the console.
          */
 
@@ -43,7 +43,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 3.
-         * 
+         *
          * Print a quoted message to the console.
          */
 
@@ -51,7 +51,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 4.
-         * 
+         *
          * Print a face to the console.
          */
 	System.out.println("   \\\\\\\\\\ ");
@@ -63,15 +63,15 @@ public class ProblemSet0{
 
         /*
          * Exercise 5.
-         * 
+         *
          * Print a to-do list to the console.
          */
 
      	System.out.println("Here's today's to-do list: \n \t- Go food shopping \n\t- Mow the lawn\n");
-        
+
 	/*
          * Exercise 6.
-         * 
+         *
          * Print a square of text to the console.
          */
 
@@ -79,7 +79,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 7.
-         * 
+         *
          * Print a countdown to the console.
          */
 
@@ -87,7 +87,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 8.
-         * 
+         *
          * Print three diamonds to the console.
          */
 
@@ -100,7 +100,7 @@ public class ProblemSet0{
 
         /*
          * Exercise 9.
-         * 
+         *
          * Print an encouraging message to the console.
          */
 
@@ -108,12 +108,12 @@ public class ProblemSet0{
 
         /*
          * Exercise 10.
-         * 
+         *
          * Print a plus sign to the console.
          */
-	
+
          System.out.println("\n\t+---+\n\t|\t|");
-   	 System.out.println("+---+\t+---+\n|\t\t\t|\n+---+\t+---+");
-   	 System.out.println("\t|\t|\n\t+---+\n");
+   	     System.out.println("+---+\t+---+\n|\t\t\t|\n+---+\t+---+");
+     	   System.out.println("\t|\t|\n\t+---+\n");
 }
 }
